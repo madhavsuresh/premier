817ecca7be27ac806a66a6124e7c6d55a3da83d1
Madhav Suresh <madhav.a.suresh@gmail.com>
2013-04-06 03:17:06 -0500

    added simple features

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,20 @@
-from flask.ext.wtf import Form,DecimalField,validators
+from flask.ext.wtf import Form,DecimalField,TextAreaField,IntegerField,validators
+
+comment_string = '''1)Knowledge of topic:
+2)Methods and Content:
+3)Clarity of Presentation:
+4)Accessibility:
+5)Research Contributions:
+6)Overall Presentation:
+'''
 
 class FeedbackForm(Form):
-    knowl_top = DecimalField(u'Knowledge of Topic (20%)',[validators.required()])
-    meth_cont = DecimalField(u'Methods and content (20%)',[validators.required()])
-    clarity = DecimalField(u'Clarity of Presentation (20%)',[validators.required()])
-    access = DecimalField(u'Accessibility (20%)',[validators.required()])
-    research = DecimalField(u'Research Contributions (10%)',[validators.required()])
-    overall = DecimalField(u'Overall Presentation (20%)',[validators.required()])
+    pres_num = IntegerField(u'Presenter Number',[validators.required(),validators.NumberRange(min=0,max=300)])
+    jud_num = IntegerField(u'Judge Number',[validators.required(),validators.NumberRange(min=0,max=300)])
+    knowl_top = DecimalField(u'Knowledge of Topic (20%)',[validators.required(),validators.NumberRange(min=1,max=7)])
+    meth_cont = DecimalField(u'Methods and content (20%)',[validators.required(),validators.NumberRange(min=1,max=7)])
+    clarity = DecimalField(u'Clarity of Presentation (20%)',[validators.required(),validators.NumberRange(min=1,max=7)])
+    access = DecimalField(u'Accessibility (20%)',[validators.required(),validators.NumberRange(min=1,max=7)])
+    research = DecimalField(u'Research Contributions (10%)',[validators.required(),validators.NumberRange(min=1,max=7)])
+    overall = DecimalField(u'Overall Presentation (20%)',[validators.required(),validators.NumberRange(min=1,max=7)])
+    comments = TextAreaField(u'Comments',default=comment_string)
--- app/views.py
+++ app/views.py
@@ -1,10 +1,49 @@
-from flask import render_template
+from flask import render_template,redirect
 from app import app
 from forms import FeedbackForm
+from app.database import db_session
+from app.models import Feedback
 
-@app.route('/')
-@app.route('/index')
-def index():
+@app.teardown_request
+def shutdown_session(exception=None):
+    db_session.remove()
+
+def form_to_dict(form):
+    ret = {}
+    for f in form:
+        ret[f.name] = f.data
+    return ret
+
+def handle_form_data(arg_d):
+    f = Feedback(arg_dict=arg_d)
+    db_session.add(f)
+    db_session.commit()
+
+        
+
+@app.route('/',methods = ['GET','POST'])
+def enter():
 #hacky hack
     form = FeedbackForm(csrf_enabled = False)
+    if form.validate_on_submit():
+        print 'woot!'
+        ad = form_to_dict(form)
+        handle_form_data(ad)
+        return redirect('/')
     return render_template('form.html',form = form)
+
+@app.route('/presenters/<pres>')
+def show_stats(pres):
+    pres = Feedback.query.filter_by(pres_num=pres).all()
+    print pres
+    if len(pres) > 0:
+        return 'I\'ll get it to you'
+    else:
+        return 'wat'
+
+@app.route('/viewall')
+def viewall():
+    pres = Feedback.query.all()
+    print pres
+    return 'heh'
+
